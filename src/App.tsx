9c603d24353fd3b752f4b83e58d4fede623ae421
Nicholas McMaster <nmcmaster@gmail.com>
2022-10-22 16:24:52 -0400

    first comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [color, setColor] = useState('#84cc16');
+
+	return (
+		<div className="App">
+			<button onClick={() => setColor('#3b82f6')}>click</button>
+			<motion.div
+				initial={{ backgroundColor: '#84cc16' }}
+				animate={{ backgroundColor: color }}
+        transition={{duration: 5}}
+				className="h-10 w-8"
+			/>
+		</div>
+	);
 }
 
 export default App;

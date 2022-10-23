7dcd48a626f6a34077b3c1b1f8af6f105ca8690e
Nicholas McMaster <nmcmaster@gmail.com>
2022-10-23 16:13:55 -0400

    draw the board

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import './App.css';
-import { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
+import { twColors500 } from './color/tailwind';
+
+
+const sixteen = Array(16).fill(undefined);
+const randoms = sixteen.map((x) => Math.floor(Math.random() * 16));
+
+interface SquareProps {
+	colorHex: string;
+}
+
+const Board = () => {
+	return (
+		<div
+			style={{ width: '50vw', height: '50vw', marginTop: '5%' }}
+			className="mx-auto shadow-xl flex flex-wrap"
+		>
+			{randoms.map((x, i) => (
+				<Square key={i} colorHex={twColors500[x]} />
+			))}
+		</div>
+	);
+};
+
+const Square: React.FC<SquareProps> = ({ colorHex }) => {
+	return (
+		<motion.div
+			className="w-1/4 h-1/4"
+			initial={{ backgroundColor: colorHex }}
+		/>
+	);
+};
 
 function App() {
-	const [color, setColor] = useState('#84cc16');
+
+	console.log(randoms);
 
 	return (
 		<div className="w-screen h-screen">
-			<div
-				style={{ width: '50vw', height: '50vw', marginTop: '12%' }}
-				className="mx-auto shadow-xl"
-			>
-				<div className='flex h-1/4'>
-					<div className="w-1/4 h-1/4 bg-slate-900" />
-					<div className="w-1/4 h-1/4 bg-slate-700" />
-					<div className="w-1/4 h-1/4 bg-slate-500" />
-					<div className="w-1/4 h-1/4 bg-slate-300" />
-				</div>
-			</div>
+			<Board />
 		</div>
 	);
 }
---
+++ src/color/tailwind.ts
@@ -0,0 +1,20 @@
+export const twColors500 = [
+	// Tailwind colors at 500 intensity
+	'#ef4444', // red
+	'#f97316', // orange
+	'#f59e0b', // amber
+	'#eab308', // yellow
+	'#84cc16', // lime
+	'#22c55e', // green
+	'#10b981', // emerald
+	'#14b8a6', // teal
+	'#06b6d4', // cyan
+	'#0ea5e9', // sky
+	'#3b82f6', // blue
+	'#6366f1', // indigo
+	'#8b5cf6', // violet
+	'#a855f7', // purple
+	'#d946ef', // fuchsia
+	'#ec4899', // pink
+	'#f43f5e', // rose
+];
